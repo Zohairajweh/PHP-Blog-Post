89d0dc4c633064b3c739b4e2d790e74c7aec8f80
Zohairajweh <60945291+Zohairajweh@users.noreply.github.com>
2020-04-30 22:32:30 -0700

    Add style

--- includes/blog.Class.php
+++ includes/blog.Class.php
@@ -18,11 +18,11 @@ class Blog {
     ob_start(); 
     ?>
       <dl>
-        <dt>id</dt>
+        <dt>ID : </dt>
         <dd><?php echo $this->id; ?></dd>
-        <dt>title</dt>
+        <dt>Title :</dt>
         <dd><?php echo $this->title; ?></dd>
-        <dt>content</dt>
+        <dt>Content : </dt>
         <dd><?php echo $this->content; ?></dd>
       </dl>
     <?php 
--- index.php
+++ index.php
@@ -1,13 +1,21 @@
 <?php
+
   ini_set( 'display_errors', 1 );
   ini_set( 'display_startup_errors', 1 );
   error_reporting( E_ALL );
   include './includes/Blogs.Class.php';
+  
+  if ( isset( $_POST ) && !empty( $_POST ) ) 
+  {
+   
+    }
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./style.css">
+
   <title>Blog</title>
 </head>
 <body>
@@ -17,21 +25,15 @@
     $blogs->output();
   ?>
 
-  <h2>find blogs</h2>
-  <p>blogs</p>
- 
-  <form action="index.php" method="post">
- <p>Enter you blogs ID : <input type="text" name="name" /></p>
- <p><input type="submit" /></p>
-</form>
+  <h2>find blogs :</h2>
+  <p>blogs:</p>
+  <div> 
+<?php 
 
-Your blog is : <?php  $blogs->findBlogByIndex($_POST['name']); ?>
-
-
-   <?php
-
-  $blogs->findBlogByIndex(4);
+     $blogs->findBlogByIndex(21)
+     
 ?>
+</div>
 
 </body>
 </html>
